010d52e00d80742f3bfe644a1f9283aa9dd232b0
Patrick Santos <patrick.sg.santos@gmail.com>
2022-11-28 10:03:54 -0300

    for each tests in javascript

---
+++ foreach.js
@@ -0,0 +1,22 @@
+// const imgs = document.querySelectorAll("img");
+
+// imgs.forEach((item) => {
+//   console.log(item);
+// });
+
+const paragrafos = document.querySelectorAll("p");
+
+// paragrafos.forEach((item) => console.log(item.innerText));
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
